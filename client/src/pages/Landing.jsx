6ff550eaf7e5b327dca91bbe24b95489ef3ea91f
Kushal Goel <kushalgoel786@gmail.com>
2023-09-11 16:34:24 -0400

    Improved the UI of Landing Page

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -4,16 +4,21 @@ const Landing = () => {
   return (
     <main className="bg-slate-800 text-white flex flex-col min-h-screen justify-center items-center">
       <h1 className="text-fuchsia-500 text-4xl font-bold mb-4">CloudX</h1>
-      <p className="text-xl mb-16">Store and share your files easily!</p>
+      <p className="text-xl mb-12">Store and share your files easily!</p>
       <Link
         to="/dashboard"
-        className="bg-fuchsia-600 hover:bg-fuchsia-700 text-white font-bold py-2 px-4 rounded">
+        className="mb-3 bg-fuchsia-600 hover:bg-fuchsia-700 text-white font-bold py-2 px-4 rounded">
         Dashboard
       </Link>
-      <div className="flex">
-        <Link to="/register">Register</Link>
-        <Link to="/login">Login</Link>
-      </div>
+      <p className="text-lg font-medium">
+        <Link to="/register" className="transition hover:text-fuchsia-500">
+          Register
+        </Link>
+        &nbsp;or&nbsp;
+        <Link to="/login" className="transition hover:text-fuchsia-500">
+          Login
+        </Link>
+      </p>
     </main>
   );
 };
