d89a0638bcc7299cc7ca1e1175b5c412bf20569d
Kushal Goel <kushalgoel786@gmail.com>
2023-09-13 12:52:13 -0400

    Rounded the Logout Button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="bg-fuchsia-700 flex justify-between items-center px-10 h-12 text-white">
       <div>CloudX</div>
-      <button onClick={logoutUser} className="px-2 border">
+      <button onClick={logoutUser} className="px-2 border rounded">
         Logout
       </button>
     </nav>
